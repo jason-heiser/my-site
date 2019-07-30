cfc8f7be5f5ba825134ecf1fe773530a43aeef82
Jason Heiser <jason.heiser@blueearth.net>
2019-07-30 18:25:49 -0500

    Swipe.

--- www/resources/bloop.js
+++ www/resources/bloop.js
@@ -40,8 +40,19 @@ var portfolio = (function() {
     return directory;
   }
 
+  function swipeHandler(event) {
+    runCarousel(false);
+    if (event.type === 'touchstart') {
+      startX = event.touches[0].clientX;
+    } else {
+      var endX = event.changedTouches[0].clientX;
+      var newIndex = savedIndex + (startX !== endX ? startX < endX ? -1 : 1 : 0);
+      goTo(0 > newIndex ? 0 : newIndex > items.length - 1 ? items.length - 1 : newIndex);
+    }
+  }
+
   function goTo(index) {
-    savedIndex = index > items.length - 1 ? 0 : index;
+    savedIndex = index;
     items.forEach((element, itemIndex) => {
       var directory = Array.from(document.querySelectorAll('ul.directory li'));
       directory[itemIndex].setAttribute('class', savedIndex === itemIndex ? 'active' : '');
@@ -54,7 +65,7 @@ var portfolio = (function() {
   function runCarousel(yes) {
     clearInterval(interval);
     if (yes) {
-      interval = setInterval(() => goTo(savedIndex + 1), 6000);
+      interval = setInterval(() => goTo(savedIndex + 1 > items.length - 1 ? 0 : savedIndex + 1), 6000);
     }
   }
 
@@ -65,12 +76,16 @@ var portfolio = (function() {
   var items = vessel.querySelectorAll('li');
   var gear = document.querySelector('section.portfolio img');
   var directory = makeDirectory(portfolio, items);
+  var startX = 0;
   vessel.style.overflow = 'hidden';
 
   [directory, vessel].forEach(element => {
     ['mouseover'].forEach(event => element.addEventListener(event, () => runCarousel(false)));
     ['mouseout'].forEach(event => element.addEventListener(event, () => runCarousel(true)));
   });
+
+  vessel.addEventListener('touchstart', swipeHandler, { passive: true });
+  vessel.addEventListener('touchend', swipeHandler, { passive: true });
 
   setTimeout(() => runCarousel(true), 5000); // Delayed until the happy cloud stops fidgeting.
   goTo(0); // Tee up directory highlight for first item.
