6a355c74a2d79240e7bad92311b0b33fafd2bfde
Jason Heiser <jason.heiser@blueearth.net>
2019-08-01 16:15:26 -0500

    More stuff.

--- www/resources/bloop.js
+++ www/resources/bloop.js
@@ -7,6 +7,7 @@
     $$ |  $$ |$$ |  $$ |
     \$$$$$$  |$$ |  $$ |
      \______/ \__|  \__|
+
      Jason Heiser - 2019
  */
 
@@ -46,9 +47,22 @@ var portfolio = (function() {
       startX = event.touches[0].clientX;
     } else {
       var endX = event.changedTouches[0].clientX;
-      var newIndex = savedIndex + (startX !== endX ? startX < endX ? -1 : 1 : 0);
-      goTo(0 > newIndex ? 0 : newIndex > items.length - 1 ? items.length - 1 : newIndex);
+      var newIndex = fenceIndex(savedIndex + (startX !== endX ? startX < endX ? -1 : 1 : 0));
+      goTo(newIndex);
     }
+  }
+
+  function keyHandler(event) {
+    runCarousel(false);
+    if (event.key === 'ArrowLeft') {
+      goTo(fenceIndex(savedIndex - 1));
+     } else if (event.key === 'ArrowRight') {
+      goTo(fenceIndex(savedIndex + 1));
+    }
+  }
+
+  function fenceIndex(newIndex) {
+    return 0 > newIndex ? 0 : newIndex > items.length - 1 ? items.length - 1 : newIndex;
   }
 
   function goTo(index) {
@@ -77,15 +91,17 @@ var portfolio = (function() {
   var gear = document.querySelector('section.portfolio img');
   var directory = makeDirectory(portfolio, items);
   var startX = 0;
-  vessel.style.overflow = 'hidden';
 
   [directory, vessel].forEach(element => {
     ['mouseover'].forEach(event => element.addEventListener(event, () => runCarousel(false)));
     ['mouseout'].forEach(event => element.addEventListener(event, () => runCarousel(true)));
   });
 
+  window.addEventListener('keyup', keyHandler);
   vessel.addEventListener('touchstart', swipeHandler, { passive: true });
   vessel.addEventListener('touchend', swipeHandler, { passive: true });
+  vessel.addEventListener('touchmove', e => e.preventDefault(), { passive: false });
+  vessel.style.overflow = 'hidden';
 
   setTimeout(() => runCarousel(true), 5000); // Delayed until the happy cloud stops fidgeting.
   goTo(0); // Tee up directory highlight for first item.
